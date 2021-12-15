f28c720b1e6d999828ef7bce3e35ccf5301e6647
Saul Ramirez <fernando.ramire@hotmail.com>
2021-12-15 09:41:02 -0600

    :bento: adding consts

---
+++ constant/index.js
@@ -0,0 +1,10 @@
+const socketActions = {
+  ON_CONNECTION: 'connection',
+  CURRENT_BANDS: 'current-bands',
+  VOTE_BANDS: 'vote-band',
+  ADD_BAND: 'add-band',
+  CHANGE_BAND_NAME: 'change-name',
+  DELETE_BAND: 'delete-band',
+};
+
+module.exports = { socketActions };
